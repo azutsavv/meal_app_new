e857a8a32c465255a30d4fc227178a9dc20c0873
Utsav Singh <azutsav2002@gmail.com>
2023-10-01 15:03:04 +0530

    all screen has been built , now working upon navigation bar

--- lib/Screenn/meal_detail_screen.dart
+++ lib/Screenn/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_meal_app/main.dart';
 import 'package:new_meal_app/models/meal.dart';
 
 class mealDetailScreen extends StatelessWidget {
@@ -15,12 +16,56 @@ class mealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl,
-      width: double.infinity,
-      height: 300,
-      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
       
-      ),
+            const SizedBox(height: 14,),
+      
+            Text('Ingredients',
+             style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
+            
+            const SizedBox(height: 14,),
+      
+            for(final ingredients in meal.ingredients)
+              Text(ingredients,
+                style:Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.white
+                ),),
+      
+            const SizedBox(height: 14,),
+      
+            Text('Steps',
+             style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
+            
+            const SizedBox(height: 14,),
+      
+            for(final steps in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10 , horizontal: 20),
+                child: Text(steps,
+                  textAlign: TextAlign.center,
+                  style:Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Colors.white
+                  ),),
+              ),
+      
+      
+      
+          ],
+        ),
+      )
     );
   }
 }
